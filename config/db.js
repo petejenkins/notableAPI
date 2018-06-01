33a5abf424afd6f9bb296a7b6bbae1a4e58fe206
Peter Jenkins <Peter.Jenkins@bjss.com>
2018-06-01 09:20:50 +0100

    First commit

---
+++ config/db.js
@@ -0,0 +1,4 @@
+
+module.exports = {
+  url : 'mongodb://dbeditor:Password123@ds016098.mlab.com:16098/notable'
+};
---
+++ server.js
@@ -0,0 +1,24 @@
+
+
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const db             = require('./config/db');
+
+const app            = express();
+
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//require('./app/routes')(app, {});
+
+//var url = 'mongodb://dbeditor:Password123@ds016098.mlab.com:16098/notable';
+// db.url
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
+})
